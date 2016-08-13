2e73e9e24ec507b4326488e81b6dff0d1826afe1
yogeshdp <pyogesh092@gmail.com>
2016-08-13 15:11:56 +0530

    intermidiate commit

--- DatabaseDemo/app/src/main/java/com/example/yogeshpatil/databasedemo/MainActivity.java
+++ DatabaseDemo/app/src/main/java/com/example/yogeshpatil/databasedemo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.yogeshpatil.databasedemo;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.example.yogeshpatil.databasedemo", Context.MODE_PRIVATE);
+
+        sharedPreferences.edit().putString("username", "Yogesh").apply();
+        sharedPreferences.edit().putString("password", "Patil").apply();
+
+        String Username = sharedPreferences.getString("username", "");
+        String Password = sharedPreferences.getString("password", "");
+
+        Log.i("Username", Username);
+        Log.i("Password", Password);
+
     }
 }
