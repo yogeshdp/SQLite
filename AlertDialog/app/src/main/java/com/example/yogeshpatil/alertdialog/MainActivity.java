8b4534ecd8811a4e8646c85ce43b2d8926fd93fd
yogeshdp <pyogesh092@gmail.com>
2016-08-13 15:45:59 +0530

    alert dialog add

--- AlertDialog/app/src/main/java/com/example/yogeshpatil/alertdialog/MainActivity.java
+++ AlertDialog/app/src/main/java/com/example/yogeshpatil/alertdialog/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.yogeshpatil.alertdialog;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,5 +29,18 @@ public class MainActivity extends AppCompatActivity {
 
         Log.i("username", username);
 
+        new AlertDialog.Builder(this)
+                .setIcon(R.drawable.ic_launcher)
+                .setTitle("Are you sure???")
+                .setMessage("Do you definitely want to do this!!!")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        Log.i("Button Tapped", "Yes");
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
     }
 }
